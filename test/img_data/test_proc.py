dfd415a4d057242268f420a20ccb3bc384662c08
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-12-26 21:13:46 +0900

    testコードを修正

--- test/img_data/test_proc.py
+++ test/img_data/test_proc.py
@@ -2,7 +2,7 @@ import rotate_ocr.process_image as process_image
 
 
 def test_process_image():
-    process_image.process_image("./test/img_data/test_data.png", "output")
+    process_image.process_image("./test/img_data/test_data.png", "./output", "tep", "./output/output_size.csv")
 
 if __name__ == "__main__":
     test_process_image()
