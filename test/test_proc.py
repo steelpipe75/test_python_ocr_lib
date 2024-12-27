7dd7b7457d012ca76bfa0627a954e6af3444a1ea
Yutaka Nishizaki <steelpipe75@gmail.com>
2024-12-28 00:23:56 +0900

    テストコードを改変しやすいように引数ごとに改行

--- test/test_proc.py
+++ test/test_proc.py
@@ -2,7 +2,12 @@ import rotate_ocr.process_image as process_image
 
 
 def test_process_image():
-    process_image.process_image("./test/img_data/test_data.png", "./output", "tep", "./output/output_size.csv")
+    process_image.process_image(
+            "./test/img_data/test_data.png",
+            "./output",
+            "tep",
+            "./output/output_size.csv",
+        )
 
 if __name__ == "__main__":
     test_process_image()
